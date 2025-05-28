870581f50750191314aedfbb847e0499f93d3ea2
Arslan1211 <Arslan1211@gmail.com>
2025-05-28 09:44:23 +0300

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
@@ -1,15 +1,13 @@
 package ru.yandex.practicum.filmorate.service;
 
+import java.time.Instant;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Event;
 import ru.yandex.practicum.filmorate.model.enumeration.EventOperation;
 import ru.yandex.practicum.filmorate.model.enumeration.EventType;
-
-import java.time.Instant;
-import java.util.List;
 import ru.yandex.practicum.filmorate.repository.impl.FeedDbStorage;
 
 @Slf4j
